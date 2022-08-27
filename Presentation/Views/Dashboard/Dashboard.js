5cfc1a32f08e39cde35c27f9005b16bc6e8bd2ea
Md Monir Hossain Showrav <61703809+shourov-sorkar@users.noreply.github.com>
2022-08-28 00:53:18 +0600

    :sparkles: Drawer Added in Dashboard

--- Presentation/Views/Dashboard/Dashboard.js
+++ Presentation/Views/Dashboard/Dashboard.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { useRouter } from "next/router";
+import DashBoardDesign from "./DashBoardDesign";
 export default function Dashboard() {
-  const router = useRouter();
   return (
     <div>
-      <Button onClick={() => router.push("/post/postlist")}>Hello World!</Button>
+      <DashBoardDesign />
     </div>
   );
 }
